c71e0bbab9626fbf4b71ad7e10c21da99da2a82f
jaehyung Lim <jaehyunglimsp15@gpa.ac.kr>
2015-10-07 13:58:55 +0900

    Encryption program

---
+++ SpyProgram.java
@@ -0,0 +1,30 @@
+/*
+ * Project: SpyProgram.java
+ * Description: A program to encrypt and decrypt password or message Strings
+ * Name: Jaehyung Lim
+ * Date: Oct 7, 2015
+ */
+
+import java.util.Scanner;
+
+public class SpyProgram {
+  
+  public static void main( String[] args) {
+  
+    Scanner s = new Scanner( System.in );
+    System.out.println( "Enter a secret message: " );
+    String secret = s.nextLine();
+    System.out.println( "To encrypt: " + secret );
+    
+    Encryption enc = new Encryption();
+    String encStr = enc.encrypt( secret ); // Encrpyt the original String
+    System.out.println( "Encrypted: " + encStr );
+    
+    String decStr = enc.decrypt( encStr );
+    System.out.println( " Decrypted: " + decStr ); // Decrypt the encrypted String
+    
+    
+    
+  } // end main method
+ 
+} // end SpyProgram
